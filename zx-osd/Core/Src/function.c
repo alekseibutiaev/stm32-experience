4820a9ea82c3e8eeea1846b3151890b4a54c0eab
Butiaev Aleksei <alexsyrezerv@mail.ru>
2024-05-21 08:31:07 +0300

    add ΜΨΟ

--- zx-osd/Core/Src/function.c
+++ zx-osd/Core/Src/function.c
@@ -9,34 +9,39 @@
 #include "spi.h"
 #include "gpio.h"
 
-uint8_t buf[] = {0xAA, 0xFF, 0x55, 0x00};
-static int a = 0;
+#define array_size(__a__) sizeof(__a__)/sizeof(*__a__)
+
+uint8_t buf[] = {0x80, 0xAA, 0xFF, 0x55, 0x00, 0x01, 0x00};
 
 void function() {
 
-	HAL_GPIO_TogglePin(led_GPIO_Port, led_Pin);
-  HAL_SPI_Transmit_DMA(&hspi1, buf, 4);
-  for(a = 0; a < 32; ++a) {
-    HAL_GPIO_TogglePin(spi_clk_GPIO_Port, spi_clk_Pin);
-    HAL_GPIO_TogglePin(spi_clk_GPIO_Port, spi_clk_Pin);
-  }
-  HAL_Delay(10);
+	HAL_GPIO_WritePin(led_GPIO_Port, led_Pin, GPIO_PIN_SET);
+  HAL_Delay(1);
+	HAL_SPI_Transmit_DMA(&hspi1, buf, array_size(buf));
+ // HAL_Delay(1);
 }
 
+
+void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef* hspi) {
+//	HAL_GPIO_TogglePin(led_GPIO_Port, led_Pin);
+//	a = 0;
+	hspi->Instance->DR = 0;
+  HAL_GPIO_WritePin(led_GPIO_Port, led_Pin, GPIO_PIN_RESET);
+	return;
+}
 
 void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi) {
 
 }
 
-void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi) {
-	return;
-}
 
 void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi) {
 
 }
 
 void HAL_SPI_TxHalfCpltCallback(SPI_HandleTypeDef *hspi) {
+//	HAL_GPIO_TogglePin(led_GPIO_Port, led_Pin);
+//	a = 0;
 	return;
 }
 
