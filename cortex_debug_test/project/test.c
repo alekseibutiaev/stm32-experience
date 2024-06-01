c710fa7b0ed0cf8d64cc07ba034d36a76db2c1f8
Butiaev Aleksei <alexsyreserv@mail.ru>
2024-06-01 11:59:26 +0300

    Serial Wire Out (SWo) is starting migrate to openocd-0.12.0

--- cortex_debug_test/project/test.c
+++ cortex_debug_test/project/test.c
@@ -13,7 +13,11 @@ int __io_putchar(int ch) {
 
 void test(void) {
   static int a = 0;
-  HAL_GPIO_TogglePin(led0_GPIO_Port, led0_Pin);
+  uint32_t dbg = ((ITM->TCR & ITM_TCR_ITMENA_Msk) != 0UL) && ((ITM->TER/* & 1UL*/) != 0UL);
+  if(dbg)
+    HAL_GPIO_TogglePin(led0_GPIO_Port, led0_Pin);
+  else
+    HAL_GPIO_WritePin(led0_GPIO_Port, led0_Pin, GPIO_PIN_SET);
   HAL_GPIO_TogglePin(led1_GPIO_Port, led1_Pin);
   HAL_Delay(1000);
   printf("SWO out %d\n", a++);
