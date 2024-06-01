5e4ebd2e2ed40aa36605c8e8f3b160adf016e604
Butiaev Aleksei <alexsyreserv@mail.ru>
2024-06-01 03:30:40 +0300

    add openocd config with diferent reset type add Serial Wire Viever (SWV) | Serial Wire Out (SWO) out pin have Serial Wire Viever (SWV) | Serial Wire Out (SWO) response fix linker warning fix version information script

--- cortex_debug_test/project/test.c
+++ cortex_debug_test/project/test.c
@@ -1,9 +1,20 @@
+#include <stm32f1xx.h>
+
+#include <stdio.h>
 
 #include <main.h>
 
+#if 1
+int __io_putchar(int ch) {
+  ITM_SendChar(ch);
+  return ch;
+}
+#endif
 
 void test(void) {
+  static int a = 0;
   HAL_GPIO_TogglePin(led0_GPIO_Port, led0_Pin);
   HAL_GPIO_TogglePin(led1_GPIO_Port, led1_Pin);
   HAL_Delay(1000);
+  printf("SWO out %d\n", a++);
 }
